adf3932f9d59c99c4eb54b05ed5c008c2a9ac5b1
h0nt3d <akanoim@gmail.com>
2025-11-22 13:14:36 -0400

    Implemnted files and directory listing - Modified FileList.jsx

--- r4zor_frontend/src/components/FileList.jsx
+++ r4zor_frontend/src/components/FileList.jsx
@@ -6,6 +6,7 @@ import '../styles/FileList.css';
 function FileList() {
 	const [files, setFiles] = useState([]);
 	const [error, setError] = useState(null);
+	const [expandedDirs, setExpandedDirs] = useState([]);
 	const host = import.meta.env.VITE_HOST_ADDRESS;
 
 	useEffect(() => {
@@ -20,19 +21,54 @@ function FileList() {
 			.catch(error => setError(error.message));
 	}, []);
 
+	const toggleDirectory = (dirName) => {
+		setExpandedDirs(prevState => {
+			if (prevState.includes(dirName)) {
+				return prevState.filter(item => item !== dirName);
+			}
+			else {
+				 return [...prevState, dirName];
+			}
+		});
+	};
+
+	const renderFiles = (items) => {
+		const directories = items.filter(item => item.type === 'directory');
+		const files = items.filter(item => item.type === 'file');
+
+		return (
+			<ul>
+			{directories.map((item, index) => {
+				const isExpanded = expandedDirs.includes(item.name);
+				return (
+					<li key={index} className="fileItem">
+						<div
+						className="fileNameFolder"
+						onClick={() => toggleDirectory(item.name)}
+						>
+							{isExpanded ? '[-]' : '[+]'} {item.name}
+						</div>
+						{isExpanded && item.children && renderFiles(item.children)}
+					</li>
+				);
+			})}
+
+			{files.map((item, index) => (
+				<li key={index} className="fileItem">
+					<div className="fileName">{item.name}</div>
+				</li>
+			))}
+			</ul>
+		);
+	};
+
 	return(
 		<div className="fileList">
 			<div className="filesHeader">
 				<h1>FILES</h1>
 			</div>
 			<div className="fileItems">
-				<ul>
-					{files.map((file, index) => (
-						<li key={index} className="fileItem">
-							{file}
-						</li>
-					))}
-				</ul>
+				{files.length > 0 ? renderFiles(files) : <p>No files found</p>}
 			</div>
 		</div>
 	);
