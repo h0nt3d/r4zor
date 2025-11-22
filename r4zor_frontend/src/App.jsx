a1717939491d78b327aa84393f58d7013d220428
h0nt3d <akanoim@gmail.com>
2025-11-22 11:07:05 -0400

    Added header for file listing - Added FileList.jsx - Added FileList.css - Modified App.jsx - Modified Upload.css

--- r4zor_frontend/src/App.jsx
+++ r4zor_frontend/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import Upload from './components/Upload'
+import FileList from './components/FileList'
 
 function App() {
 	return (
 		<div className="">
 			<Upload />
+			<FileList />
 		</div>
 	);
 }
---
+++ r4zor_frontend/src/components/FileList.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import '../styles/FileList.css';
+
+function FileList() {
+	return(
+		<div className="filesHeader">
+			<h1>FILES</h1>
+		</div>
+	);
+}
+
+export default FileList;
